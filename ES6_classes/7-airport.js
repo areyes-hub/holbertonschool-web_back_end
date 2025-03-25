2e1a9e7fc459ad302d9a0c1e6f2fcb71f123aa92
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-25 16:05:58 -0400

    Inheritance and Airport class

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,21 @@
+export default class Airport {
+    constructor(name, code) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        if (typeof code !== 'string') {
+            throw new TypeError('Code must be a string');
+        }
+        this._name = name;
+        this._code = code;
+    }
+
+
+    toString() {
+        return `[object ${this._code}]`;
+    }
+
+    [Symbol.toStringTag]() {
+        return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+    }
+}
