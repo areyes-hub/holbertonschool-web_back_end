c144425b5ca058c1ec5ad94bd139b0a2f62165cd
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-25 09:44:24 -0400

    Getters, setters and methods

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' && code.length > 1) {
+      throw new TypeError('Code must be a one character string');
+    }
+    this._code = code;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string' && code.length > 1) {
+      throw new TypeError('Code must be a one character string');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
