27c0f0fdf77d3a0ce5d9fcf77ac97170b48e5de2
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-26 16:23:30 -0400

    Refactor code

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,13 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    try {
-      const task = true;
-      const task2 = false;
-    } catch (err) {
-
-    }
+    return [task, task2];
   }
-
   return [task, task2];
 }
