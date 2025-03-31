95d41ed10bc7fa60384758193a590e4f365d6a7d
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-31 06:07:48 -0400

    List of students

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, first_name: 'Guillaume', location: 'San Francisco' },
+    { id: 2, first_name: 'James', location: 'Columbia' },
+    { id: 5, first_name: 'Serena', location: 'San Francisco' },
+  ];
+}
