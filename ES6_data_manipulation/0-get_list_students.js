a5dc2949f582bb06fafe4a0f051cb61acc503111
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-03-31 06:13:19 -0400

    Refactor Code

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
 export default function getListStudents() {
-  return [
-    { id: 1, first_name: 'Guillaume', location: 'San Francisco' },
-    { id: 2, first_name: 'James', location: 'Columbia' },
-    { id: 5, first_name: 'Serena', location: 'San Francisco' },
-  ];
+    let students = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    return students;
 }
