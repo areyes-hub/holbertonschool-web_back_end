912e53c6d39b8b7e8c43fcd91efb0d83d911c27c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-04-01 14:35:18 -0400

    Refactor code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,5 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.forEach((result) => [`{status: ${result.status},
-        value: ${result.status === 'fulfilled' ? result.value : result.reason}},`]));
+    .then((values) => console.log(values));
 }
