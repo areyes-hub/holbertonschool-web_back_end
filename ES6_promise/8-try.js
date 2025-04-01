478bf9991c49afee6b1fcc93ea62bd0564a9458c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2025-04-01 15:26:20 -0400

    Try

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  return Promise.try(numerator / denominator)
-    .then((result) => console.log(result))
-    .catch(() => {
-      throw new Error('cannot divide by 0');
-    });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
