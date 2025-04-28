d148abd330f38808ecf8b870db086e62415a0f0d
owner <airs@gmail.com>
2025-04-28 16:14:39 -0400

    Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -55,7 +55,7 @@ class Server:
             return []
 
         return dataset[start_index:end_index]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """
         Returns pagination metadata as a dictionary.
