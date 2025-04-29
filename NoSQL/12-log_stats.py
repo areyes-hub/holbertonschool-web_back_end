178f0d08056c8841c62ceb38d2c967489be94c8c
owner <airs@gmail.com>
2025-04-29 15:06:59 -0400

    Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""log_stats module"""
+from pymongo import MongoClient
+
+
+def nginx_stats():
+    """
+    provides some stats about Nginx logs stored in MongoDB
+    """
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Count GET requests to path=/status
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    nginx_stats()
